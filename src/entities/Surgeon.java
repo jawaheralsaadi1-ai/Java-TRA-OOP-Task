88e96af9089cc7161e383a3f43989f8f0a958fc6
jawaher <jawaheralsaadi!@gmail.com>
2026-05-12 16:15:03 +0400

    add(Surgeon) : implement specific data and update constructor.

--- src/entities/Surgeon.java
+++ src/entities/Surgeon.java
@@ -1,12 +1,32 @@
 package entities;
 
-import javax.print.Doc;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-public class   Surgeon  extends Doctor {
+public class Surgeon extends Doctor {
 
- //The Constructor with Chaining
-    public Surgeon(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee) {
-        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee);
-    }
+    //1. Add specific data
+    private int surgeriesPerformed;
+    private List<String> surgeryTypes;
+    private boolean operationTheatreAccess;
+
+    // 2. Updated Constructor
+    public Surgeon(String id, String firstName, String lastName, LocalDate dateOfBirth,
+                   String gender, String phoneNumber, String email, String address,
+                   String doctorId, String specialization, String qualification,
+                   int experienceYears, String departmentId, double consultationFee,
+                   boolean operationTheatreAccess) {
+
+        // Pass  mandatory identity data to the Doctor class
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address,
+                doctorId, specialization, qualification, experienceYears, departmentId, consultationFee);
+
+        // Initialize Surgeon-specific data
+        this.surgeriesPerformed = 0; // Starts at zero
+        this.surgeryTypes = new ArrayList<>();
+        this.operationTheatreAccess = operationTheatreAccess;
+
+}
 }

4d48282be73e67b28a9cf67c87cde7998fd4af8b
jawaher <jawaheralsaadi!@gmail.com>
2026-05-12 14:06:52 +0400

    add(main): register nurse and assign to emergency department.

--- src/Main.java
+++ src/Main.java
@@ -16,4 +16,16 @@ void main() {
     System.out.println("\n[Action] Creating Emergency Department...");
     Department emergency = new Department("DEP-EMR", "Emergency Room", 10);
 
+    // 2. REGISTER A NURSE
+    System.out.println("\n[Action] Onboarding Nurse...");
+    System.out.print("Enter Nurse First Name: ");
+    String nName = scanner.nextLine();
+
+    Nurse nurse = new Nurse(
+            UUID.randomUUID().toString(), nName, "Staff", LocalDate.of(1995, 8, 15),
+            "Female", "99112233", nName.toLowerCase() + "@hosp.com", "Muscat",
+            "NUR-001", "DEP-EMR", "Night Shift", "Registered Nurse (RN)"
+    );
+    emergency.assignNurse(nurse);
+
 }

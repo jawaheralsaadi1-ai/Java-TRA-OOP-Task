38d650f982f8c416a8ebfa3c7fed456dbf1f270a
jawaher <jawaheralsaadi!@gmail.com>
2026-05-13 14:11:08 +0400

    add(OutPatient): implement specific data and update constructor.

--- src/entities/OutPatient.java
+++ src/entities/OutPatient.java
@@ -2,10 +2,30 @@ package entities;
 
 import java.time.LocalDate;
 
-public class OutPatient extends Person {
+/*
+   1.Additional fields :
+     -visitCount (int)
+     -lastVisitDate (LocalDate)
+     -preferredDoctorId (String)
+  2. Add Override methods
+  3. Add methods :  scheduleFollowUp(), updateVisitCount()*/
 
-    //The Constructor with Chaining avoid Error
-    public OutPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address) {
+public class OutPatient extends Person {
+    // Declaring  specific data
+    private int visitCount;
+    private LocalDate lastVisitDate;
+    private String preferredDoctorId;
+
+    // Update Constructor Chaining
+    public OutPatient(String id, String firstName, String lastName, LocalDate dateOfBirth,
+                      String gender, String phoneNumber, String email, String address,
+                      int visitCount, LocalDate lastVisitDate, String preferredDoctorId) {
+        // Pass mandatory identity data to the Patient class
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
+
+        // Initialize specific data
+        this.visitCount = visitCount;
+        this.lastVisitDate = lastVisitDate;
+        this.preferredDoctorId = preferredDoctorId;
     }
 }

a47fdeee98ad01899551ad1a61b014847a30c85d
jawaher <jawaheralsaadi!@gmail.com>
2026-05-13 11:40:25 +0400

    add(GeneralPractitioner) :implement specific data and update constructor.

--- src/entities/GeneralPractitioner.java
+++ src/entities/GeneralPractitioner.java
@@ -1,10 +1,33 @@
 package entities;
-
 import java.time.LocalDate;
 
+
+
+/*GeneralPractitioner Class (extends Doctor)
+• Additional fields:
+o walkinAvailable (boolean)
+o homeVisitAvailable (boolean)
+o vaccinationCertified (boolean)
+• Override methods
+• Add: scheduleHomeVisit(), administerVaccine()*/
+
 public class GeneralPractitioner extends Doctor {
-    //The Constructor with Chaining avoid Error
-    public GeneralPractitioner(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee) {
+    // 1. Declaring specific data
+    private boolean walkinAvailable;
+    private boolean homeVisitAvailable;
+    private boolean vaccinationCertified;
+
+    //2. Update Constructor
+    public GeneralPractitioner(String id, String firstName, String lastName, LocalDate dateOfBirth,
+                               String gender, String phoneNumber, String email, String address,
+                               String doctorId, String specialization, String qualification,
+                               int experienceYears, String departmentId, double consultationFee,
+                               boolean walkinAvailable, boolean homeVisitAvailable, boolean vaccinationCertified)
+    {
+        // Pass mandatory identity data to the Doctor class
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee);
+        this.walkinAvailable = walkinAvailable;
+        this.homeVisitAvailable = homeVisitAvailable;
+        this.vaccinationCertified = vaccinationCertified;
     }
 }

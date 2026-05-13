af89561873036fab327d2f2286daacd25a2ffbce
jawaher <jawaheralsaadi!@gmail.com>
2026-05-13 14:17:05 +0400

    add(OutPatient): Setup override displayInfo and specialized methods.

--- src/entities/OutPatient.java
+++ src/entities/OutPatient.java
@@ -24,8 +24,23 @@ public class OutPatient extends Person {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
 
         // Initialize specific data
-        this.visitCount = visitCount;
-        this.lastVisitDate = lastVisitDate;
+        this.visitCount = 0; // Initial value
+        this.lastVisitDate = lastVisitDate.now(); // Sets the current date as the first visit
         this.preferredDoctorId = preferredDoctorId;
     }
+    // Add Override methods
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+    }
+
+    // Setup Specialized Methods
+
+    // Method 1
+    public void scheduleFollowUp(){}
+
+    // Method 2
+    public void updateVisitCount(){}
+
+
 }

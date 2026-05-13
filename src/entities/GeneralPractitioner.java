dee273a4d68179e0d33c9adec55a8a63e36c8017
jawaher <jawaheralsaadi!@gmail.com>
2026-05-13 12:16:52 +0400

    add(GP):implement specialized methods and override displayInfo.

--- src/entities/GeneralPractitioner.java
+++ src/entities/GeneralPractitioner.java
@@ -11,13 +11,15 @@ o vaccinationCertified (boolean)
 • Override methods
 • Add: scheduleHomeVisit(), administerVaccine()*/
 
+//Task 2.1: Specialized Doctor Type - General Practitioner (GP)
 public class GeneralPractitioner extends Doctor {
+
     // 1. Declaring specific data
     private boolean walkinAvailable;
     private boolean homeVisitAvailable;
     private boolean vaccinationCertified;
 
-    //2. Update Constructor
+    //2. Update Constructor Person -> Doctor -> GeneralPractitioner
     public GeneralPractitioner(String id, String firstName, String lastName, LocalDate dateOfBirth,
                                String gender, String phoneNumber, String email, String address,
                                String doctorId, String specialization, String qualification,
@@ -25,9 +27,40 @@ public class GeneralPractitioner extends Doctor {
                                boolean walkinAvailable, boolean homeVisitAvailable, boolean vaccinationCertified)
     {
         // Pass mandatory identity data to the Doctor class
-        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee);
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address,
+                doctorId, specialization, qualification, experienceYears, departmentId, consultationFee);
+        //Initialize specific data
         this.walkinAvailable = walkinAvailable;
         this.homeVisitAvailable = homeVisitAvailable;
-        this.vaccinationCertified = vaccinationCertified;
+        this.vaccinationCertified = true;
     }
+
+    // 3. Add Specialized Methods
+    // Add scheduleHomeVisit()
+    public void scheduleHomeVisit(){
+        if (homeVisitAvailable) {
+            System.out.println("[ [GP] Home visit scheduled successfully.");
+        } else {
+            System.out.println(" [GP] Home visits are not available for this doctor.");
+        }
+    }
+    // Add administerVaccine()
+    public void administerVaccine(){
+        if (vaccinationCertified) {
+            System.out.println("[GP] Vaccine Administering successfully");
+        } else {
+            System.out.println("[GP] Doctor is not certified to administer vaccines.");
+        }
+    }
+
+
+@Override
+public void displayInfo() {
+    // First, call the Doctor's displayInfo to show basic info
+    super.displayInfo();
+    // Then, add GP-specific info
+    System.out.println("Walk-in Available: " + walkinAvailable);
+    System.out.println("Home Visit Available: " + homeVisitAvailable);
+    System.out.println("Vaccination Certified: " + vaccinationCertified);
+}
 }

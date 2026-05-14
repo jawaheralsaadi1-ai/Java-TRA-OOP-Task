084cc466cb3091fa9fe9a99a3918ec34e4d5da42
jawaher <jawaheralsaadi!@gmail.com>
2026-05-14 13:35:57 +0400

    refactor([InPatient]) : simplify by implement Object Chaining

--- src/entities/InPatient.java
+++ src/entities/InPatient.java
@@ -1,61 +1,15 @@
 package entities;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
-
 public class InPatient extends Patient {
+    protected String roomNumber;
+    protected double dailyCharges;
 
-// What Do?
-// 1. Add specific data
-// 2. Override methods
-// 3. Add: calculateStayDuration(), calculateTotalCharges()*/
-
-
-    // Add specific  data
-    private LocalDate admissionDate;
-    private LocalTime dischargeDate;
-    private String roomNumber;
-    private String bedNumber;
-    private String admittingDoctorId;
-    private double dailyCharges;
-
-    //Update Constructor Chaining
-    public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth,
-                     String gender, String phoneNumber, String email, String address,
-                     String patientId, String bloodGroup, String emergencyContact, String insuranceId,
-                     LocalDate admissionDate, LocalTime dischargeDate, String roomNumber, String bedNumber,
-                     String admittingDoctorId, double dailyCharges) {
-        // Pass mandatory identity data to the Patient class
-        super(id, firstName, lastName, dateOfBirth, gender,
-                phoneNumber, email, address, patientId, bloodGroup,
-                emergencyContact, insuranceId);
-
-        //Initialize specific data
-        this.admissionDate = admissionDate;
-        this.dischargeDate = dischargeDate;
+    public InPatient(String firstName, String lastName, String phoneNumber,
+                     String patientId, String roomNumber, double dailyCharges) {
+        // CHAINING: Calling the Patient(...) constructor
+        super(firstName, lastName, phoneNumber, patientId);
         this.roomNumber = roomNumber;
-        this.bedNumber = bedNumber;
-        this.admittingDoctorId = admittingDoctorId;
         this.dailyCharges = dailyCharges;
+        System.out.println("DEBUG: Level 3 - InPatient initialized.");
     }
-    //  Override displayInfo
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
-
-    }
-    //  Add Specialized Methods
-
-    // Add method 1
-    public void calculateStayDuration() {
-
-    }
-
-    //Add method 2
-    public void calculateTotalCharges() {
-
-    }
-
-
 }
